8bc3a037882d9efb2ca8b0fd260a597493a109ac
SzymonFurmanczyk <209320@student.pwr.edu.pl>
2015-03-11 12:58:40 +0100

    Pliki

---
+++ prj/src/main.cpp
@@ -0,0 +1,21 @@
+/*!
+ *\file 
+ *\brief Modul glowny.
+ *
+ * Plik zawiera funkcje main.
+ */
+
+#include <cstdlib>
+#include <iomanip>
+#include <unistd.h>
+
+using namespace std;
+
+/*!
+ *\brief Funkcja glowna programu.
+ */
+int main(int argc, char **argv)
+{
+
+  return 0;
+}
