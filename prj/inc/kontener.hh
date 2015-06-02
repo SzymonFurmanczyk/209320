78f0a6abba6bd25c020fdd5ca1bcb800ecda3975
SzymonFurmanczyk <szymon@ubuntu.ubuntu-domain>
2015-06-02 18:39:28 +0200

    Drobne poprawki.

--- prj/inc/kontener.hh
+++ prj/inc/kontener.hh
@@ -1,7 +1,31 @@
-#ifdef KONTENER_HH
+#ifndef KONTENER_HH
 #define KONTENER_HH
 
+#include <vector>
+#include <list>
+#include <algorithm>
+#include <string>
+#include <iostream>
+#include <fstream>
 
+#include "boost/tuple/tuple.hpp"
+
+struct disk {
+  std::list<std::string> files;
+  long size = 0;
+};
+
+struct container {
+  std::vector<boost::tuple<long ,std::string>> files_test;
+  std::list<disk*> disks; 
+
+
+  void sort();
+  void partition();
+  void load();
+  void save();
+
+};
 
 
 #endif
